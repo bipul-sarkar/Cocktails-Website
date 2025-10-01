1638a4650023db740952591896e732ee8dd88d71
Bipul Sarkar <bipulsarkar.pro@gmail.com>
2025-10-01 17:56:12 +0530

    implemented the first part of the hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import { ScrollTrigger, SplitText } from 'gsap/all'
-import gsap from 'gsap';
-import Navbar from './components/Navbar';
+import React from "react";
+import { ScrollTrigger, SplitText } from "gsap/all";
+import gsap from "gsap";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
-gsap.registerPlugin(ScrollTrigger , SplitText);
-
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-   <main>
-    <Navbar/>
-   </main>
-  )
-}
+    <main>
+      <Navbar />
+      <Hero />
+      <div className="h-dvh bg-black"></div>
+    </main>
+  );
+};
 
-export default App
+export default App;
